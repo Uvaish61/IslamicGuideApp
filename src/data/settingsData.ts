6ae92757a19c796062e260948d7c31ddba74de85
Uvaish61 <uvaishkhanwork@gmail.com>
2026-04-27 23:35:15 +0530

    add settings options data

---
+++ src/data/settingsData.ts
@@ -0,0 +1,17 @@
+export const settingsOptions = [
+  {
+    id: 'notifications',
+    label: 'Notifications',
+    value: 'Enabled',
+  },
+  {
+    id: 'language',
+    label: 'Language',
+    value: 'English',
+  },
+  {
+    id: 'privacy',
+    label: 'Privacy',
+    value: 'Standard',
+  },
+];
