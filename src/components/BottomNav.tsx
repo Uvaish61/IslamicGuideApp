14f949815c23e4793726351f625628dabeb3b476
Uvaish61 <uvaishkhanwork@gmail.com>
2026-04-28 22:38:16 +0530

    add BottomNav component scaffold

---
+++ src/components/BottomNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const BottomNav = () => {
+  // scaffold: will render bottom navigation tabs
+  return (
+    <View style={{ height: 60, backgroundColor: 'transparent' }} />
+  );
+};
+
+export default BottomNav;
