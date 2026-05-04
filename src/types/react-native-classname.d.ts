7224f73c903a60374307a0fafc6db95eb7d9a79b
Uvaish61 <uvaishkhanwork@gmail.com>
2026-05-04 16:38:27 +0530

    type check and lint

---
+++ src/types/react-native-classname.d.ts
@@ -0,0 +1,24 @@
+import 'react-native';
+
+declare module 'react-native' {
+  interface ViewProps {
+    className?: string;
+  }
+  interface TextProps {
+    className?: string;
+  }
+  interface TextInputProps {
+    className?: string;
+  }
+  interface ScrollViewProps {
+    className?: string;
+  }
+  interface PressableProps {
+    className?: string;
+  }
+  interface SafeAreaViewProps {
+    className?: string;
+  }
+}
+
+export {};
